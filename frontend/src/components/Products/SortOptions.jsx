5327acf329f977e5ae1412dfb3bb4657b2053c35
PrathamSingh1 <prathamjii36@gmail.com>
2026-03-08 16:00:54 +0000

    Added filtering

--- frontend/src/components/Products/SortOptions.jsx
+++ frontend/src/components/Products/SortOptions.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 
 const SortOptions = () => {
-  return <div>SortOptions</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleSortChange = (e) => {
+    const sortBy = e.target.value;
+    searchParams.set("sortBy", sortBy);
+    setSearchParams(searchParams);
+  };
+  return (
+    <div className="flex items-center justify-end mr-5">
+      <select
+        id="sort"
+        className="border p-2 rounded-md focus:outline-none"
+        onChange={handleSortChange}
+        value={searchParams.get("sortBy") || ""}
+      >
+        <option value="">Default</option>
+        <option value="priceAsc">Price: Low to High</option>
+        <option value="priceDesc">Price: High to Low</option>
+        <option value="popularity">Popularity</option>
+      </select>
+    </div>
+  );
 };
 
 export default SortOptions;
